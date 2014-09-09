f5284e41fbe7f93fb1c0da0805b62859eefe4765
matrixorz <matrix56.ch@gmail.com>
2014-09-09 21:41:30 +0800

    gh-pages

--- justpic/markpic/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from markpic.models import Student,Picture5K,Log5k
-
-admin.site.register(Student)
-admin.site.register(Log5k)
-admin.site.register(Picture5K)
--- justpic/markpic/lib/cache.py
+++
@@ -1,7 +0,0 @@
-#a cron job will cache the picture in the database
-
-access_token = '21.9403b998cd9b271fa44a54199aad2949.2592000.1401980588.3875775130-1056026'
-pcs = PCS(access_token)
-
-
-
